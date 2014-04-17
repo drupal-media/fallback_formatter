a455ee62668c4994aa1fba165dfd34cc28d390df
Dave Reid <dave@davereid.net>
2014-04-17 01:18:33 -0500

    Workaround since vertical tabs doesn't work in the formatter settings form.

---
+++ fallback_formatter.admin.js
@@ -0,0 +1,49 @@
+(function ($) {
+
+Drupal.behaviors.formatterStatusStatus = {
+  attach: function (context, settings) {
+    $('.fallback-formatter-status-wrapper input.form-checkbox', context).once('fallback-formatter-status', function () {
+      var $checkbox = $(this);
+      // Retrieve the tabledrag row belonging to this filter.
+      var $row = $('#' + $checkbox.attr('id').replace(/-status$/, '-weight'), context).closest('tr');
+      // Retrieve the vertical tab belonging to this filter.
+      var tab = $('#' + $checkbox.attr('id').replace(/-status$/, '-settings'), context).data('verticalTab');
+      // Because the vertical tabs aren't currently working, just work on the
+      // fieldset itself for now.
+      var $fieldset = $('#' + $checkbox.attr('id').replace(/-status$/, '-settings'), context);
+
+      // Bind click handler to this checkbox to conditionally show and hide the
+      // filter's tableDrag row and vertical tab pane.
+      $checkbox.bind('click.filterUpdate', function () {
+        if ($checkbox.is(':checked')) {
+          $row.show();
+          $fieldset.show();
+          if (tab) {
+            tab.tabShow().updateSummary();
+          }
+        }
+        else {
+          $row.hide();
+          $fieldset.hide();
+          if (tab) {
+            tab.tabHide().updateSummary();
+          }
+        }
+        // Restripe table after toggling visibility of table row.
+        Drupal.tableDrag['fallback-formatter-order'].restripeTable();
+      });
+
+      // Attach summary for configurable filters (only for screen-readers).
+      if (tab) {
+        tab.fieldset.drupalSetSummary(function (tabContext) {
+          return $checkbox.is(':checked') ? Drupal.t('Enabled') : Drupal.t('Disabled');
+        });
+      }
+
+      // Trigger our bound click handler to update elements to initial state.
+      $checkbox.triggerHandler('click.filterUpdate');
+    });
+  }
+};
+
+})(jQuery);
