334ce9a9f3acd39371f95c7cb64c55191c3c76be
Dave Reid <dave@davereid.net>
2015-08-17 13:58:04 -0500

    Fixed fallback_formatter.admin.js.

--- fallback_formatter.admin.js
+++ fallback_formatter.admin.js
@@ -6,49 +6,51 @@
    * Attach behaviors for the display settings.
    */
   Drupal.behaviors.fallbackFormatterStatus = {
-    attach: function (context, settings) {
-    $('.fallback-formatter-status-wrapper input.form-checkbox', context).once('fallback-formatter-status', function () {
-      var $checkbox = $(this);
-      // Retrieve the tabledrag row belonging to this filter.
-      var $row = $('#' + $checkbox.attr('id').replace(/-status$/, '-weight'), context).closest('tr');
-      // Retrieve the vertical tab belonging to this filter.
-      var tab = $('#' + $checkbox.attr('id').replace(/-status$/, '-settings'), context).data('verticalTab');
-      // Because the vertical tabs aren't currently working, just work on the
-      // fieldset itself for now.
-      var $fieldset = $('#' + $checkbox.attr('id').replace(/-status$/, '-settings'), context);
+    attach: function (context) {
+      $('.fallback-formatter-status-wrapper input.form-checkbox', context).once('fallback-formatter-status').each(function () {
+        var $checkbox = $(this);
+        var formatterSelector = $checkbox.attr('data-drupal-selector').replace(/-status$/, '');
 
-      // Bind click handler to this checkbox to conditionally show and hide the
-      // filter's tableDrag row and vertical tab pane.
-      $checkbox.bind('click.filterUpdate', function () {
-        if ($checkbox.is(':checked')) {
-          $row.show();
-          $fieldset.show();
-          if (tab) {
-            tab.tabShow().updateSummary();
-          }
-        }
-        else {
-          $row.hide();
-          $fieldset.hide();
-          if (tab) {
-            tab.tabHide().updateSummary();
-          }
-        }
-        // Restripe table after toggling visibility of table row.
-        Drupal.tableDrag['fallback-formatter-order'].restripeTable();
-      });
+        // Retrieve the tabledrag row belonging to this filter.
+        var $row = $('[data-drupal-selector="' + formatterSelector + '-weight"]', context).closest('tr');
+        // Retrieve the vertical tab belonging to this filter.
+        var tab = $('[data-drupal-selector="' + formatterSelector + '-settings"]', context).data('verticalTab');
+        // Because the vertical tabs aren't currently working, just work on the
+        // fieldset itself for now.
+        var $fieldset = $('[data-drupal-selector="' + formatterSelector + '-settings"]', context);
 
-      // Attach summary for configurable filters (only for screen-readers).
-      if (tab) {
-        tab.fieldset.drupalSetSummary(function (tabContext) {
-          return $checkbox.is(':checked') ? Drupal.t('Enabled') : Drupal.t('Disabled');
+        // Bind click handler to this checkbox to conditionally show and hide the
+        // filter's tableDrag row and vertical tab pane.
+        $checkbox.bind('click.filterUpdate', function () {
+          if ($checkbox.is(':checked')) {
+            $row.show();
+            $fieldset.show();
+            if (tab) {
+              tab.tabShow().updateSummary();
+            }
+          }
+          else {
+            $row.hide();
+            $fieldset.hide();
+            if (tab) {
+              tab.tabHide().updateSummary();
+            }
+          }
+          // Restripe table after toggling visibility of table row.
+          Drupal.tableDrag['fallback-formatter-order'].restripeTable();
         });
-      }
 
-      // Trigger our bound click handler to update elements to initial state.
-      $checkbox.triggerHandler('click.filterUpdate');
-    });
-  }
-};
+        // Attach summary for configurable filters (only for screen-readers).
+        if (tab) {
+          tab.fieldset.drupalSetSummary(function (tabContext) {
+            return $checkbox.is(':checked') ? Drupal.t('Enabled') : Drupal.t('Disabled');
+          });
+        }
+
+        // Trigger our bound click handler to update elements to initial state.
+        $checkbox.triggerHandler('click.filterUpdate');
+      });
+    }
+  };
 
 })(jQuery, Drupal);
